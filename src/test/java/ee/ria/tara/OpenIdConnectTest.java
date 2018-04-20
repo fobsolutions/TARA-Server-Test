25206123a9c6a31a07f82a1466d3186b2e7ea849
naare <aare.nurm@nortal.com>
2018-04-20 16:53:28 +0300

    TARAEI-108 addition of happy path test.

--- src/test/java/ee/ria/tara/OpenIdConnectTest.java
+++ src/test/java/ee/ria/tara/OpenIdConnectTest.java
@@ -2,15 +2,12 @@ package ee.ria.tara;
 
 
 import com.nimbusds.jose.JOSEException;
-import com.nimbusds.jose.proc.BadJOSEException;
 import com.nimbusds.jwt.SignedJWT;
 import ee.ria.tara.config.IntegrationTest;
 import io.restassured.response.Response;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.text.ParseException;
 
@@ -21,7 +18,19 @@ import static org.junit.Assert.assertEquals;
 public class OpenIdConnectTest extends TestsBase {
 
     @Test
-    public void requestTokenTwiceShouldFail() throws InterruptedException, URISyntaxException, ParseException, JOSEException {
+    public void happyPathTest() throws InterruptedException, URISyntaxException, ParseException, JOSEException {
+        String authorizationCode = authenticateWithMobileId("00000266", "60001019896", 2000);
+        SignedJWT signedJWT = verifyTokenAndReturnSignedJwtObject(getIdToken(authorizationCode));
+
+        assertEquals("EE60001019896", signedJWT.getJWTClaimsSet().getSubject());
+        assertEquals("MARY ÄNN", signedJWT.getJWTClaimsSet().getJSONObjectClaim("profile_attributes").getAsString("given_name"));
+        assertEquals("O’CONNEŽ-ŠUSLIK TESTNUMBER", signedJWT.getJWTClaimsSet().getJSONObjectClaim("profile_attributes").getAsString("family_name"));
+        assertEquals("+37200000266", signedJWT.getJWTClaimsSet().getJSONObjectClaim("profile_attributes").getAsString("mobile_number"));
+    }
+
+
+    @Test
+    public void requestTokenTwiceShouldFail() throws InterruptedException, URISyntaxException {
         String authorizationCode = authenticateWithMobileId("+37200000266", "60001019896", 2000);
         getIdToken(authorizationCode);
         Response response = postToTokenEndpoint(authorizationCode);
